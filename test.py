56a0799da53dcd9511074a93bf91c0a7c4435ed3
OneAvargeCoder193 <mgiakimenko@outlook.com>
2024-08-12 18:35:49 -0400

    add antialiasing to the sun

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 img = Image.open("image.png")
 
 out = Image.new("RGBA", img.size)
+draw = ImageDraw.Draw(out)
+
 out_ring = Image.new("RGBA", img.size)
 
 for y in range(img.height):
@@ -16,6 +18,13 @@ for y in range(img.height):
             out.putpixel((x, y), (255, 255, 200, r))
         else:
             out_ring.putpixel((x, y), (255, 255, 200, r))
+
+aac = Image.new("RGBA", (img.width * 2, img.height * 2))
+draw = ImageDraw.Draw(aac)
+draw.ellipse([aac.width / 2 - 30, aac.height / 2 - 30, aac.width / 2 + 31, aac.height / 2 + 31], outline=(255, 255, 200, 255), width=2)
+
+out.alpha_composite(aac.resize(img.size))
+out_ring.alpha_composite(aac.resize(img.size))
 
 out.save("assets/textures/sun.png")
 out_ring.save("assets/textures/sun_ring.png")
